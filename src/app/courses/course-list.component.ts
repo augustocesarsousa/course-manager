e833c2e662cee8876c3c553c22f1b558ec98c128
Augusto Cesar <acs-03@hotmail.com>
2022-06-28 17:12:51 -0300

    first component

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+  styleUrls: ['./course-list.component.css'],
+})
+export class CourseListComponent implements OnInit {
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular: Forms',
+        imageUrl: '',
+        price: 29.9,
+        code: 'ABC-123',
+        duration: 20,
+        rating: 4.5,
+        releaseDate: 'December, 2, 2021',
+      },
+      {
+        id: 1,
+        name: 'Angular: HTTP',
+        imageUrl: '',
+        price: 29.9,
+        code: 'DEF-456',
+        duration: 45,
+        rating: 5,
+        releaseDate: 'December, 3, 2021',
+      },
+    ];
+  }
+}
